3d852c85c29544e8e79404e8e9c03ac3c77c0767
benwalton7 <99775014+benwalton7@users.noreply.github.com>
2022-04-02 22:51:36 -0400

    Update common.groovy

--- common.groovy
+++ common.groovy
@@ -1,1 +1,32 @@
+//@Library('LibraryName') _ 
+// Declarative pipeline
 
+@Library('LandmarkSS-sharedlibs') _
+pipeline {
+agent any 
+tools {
+    maven "maven3.8.5"
+  }
+stages{
+stage('CheckoutCode'){
+  steps{
+    git 'https://github.com/LandmakTechnology/paypal-web-app'
+}
+}
+stage("Build"){ 
+  steps{
+    common("Build")  
+}
+}
+stage("Execute SonarQube Report"){ 
+  steps{
+    common("SonarQube Report")
+}
+}
+stage("Upload Artifacts Into Nexus"){ 
+  steps{
+    common("Upload Into Nexus")
+}
+}
+}
+}
